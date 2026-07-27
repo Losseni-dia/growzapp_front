8031fd9efde0f03e32d39b79f945f6dbdfdef3a1
Losseni Dia <losdiakite@gmail.com>
2026-07-27 20:52:48 +0200

    chore: désinstallation complète de Storybook (paquets, scripts, config Vite/ESLint)

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,9 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import unusedImports from 'eslint-plugin-unused-imports'
+import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([globalIgnores(['dist']), {
@@ -26,4 +25,29 @@ export default defineConfig([globalIgnores(['dist']), {
   rules: {
     'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
   },
-}, ...storybook.configs["flat/recommended"]])
+}, {
+  files: ['**/*.{ts,tsx}'],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    reactHooks.configs.flat.recommended,
+    reactRefresh.configs.vite,
+  ],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      ecmaFeatures: { jsx: true },
+      sourceType: 'module',
+    },
+  },
+  plugins: {
+    'unused-imports': unusedImports,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]', argsIgnorePattern: '^_' }],
+  },
+}])
